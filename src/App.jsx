ac79f59c72670ad0becf07792b9cd400cca9ea67
Valen DeLeon <valeriedeleon4521@gmail.com>
2024-12-18 17:05:40 -0800

    User Auth using Auth0

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import LoginButton from "./components/LoginButton.jsx";
+import LogoutButton from "./components/LogoutButton.jsx";
 import LoginPage from "./pages/LoginPage";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <LoginPage />
