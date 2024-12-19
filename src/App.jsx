0959ec954b9f992fa840027e2a892d4661516ebb
Valen DeLeon <valeriedeleon4521@gmail.com>
2024-12-18 17:06:56 -0800

    Test Components & Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import LoginButton from "./components/LoginButton.jsx";
 import LogoutButton from "./components/LogoutButton.jsx";
-import LoginPage from "./pages/LoginPage";
+import Profile from "./components/Profile.jsx";
+import { useAuth0 } from "@auth0/auth0-react";
+// import LoginPage from "./pages/LoginPage";
 
 function App() {
+  const { isLoading, error } = useAuth0();
+
   return (
     <>
-      <LoginPage />
+      <h1>Auth0 Login</h1>
+      {error && <p>Authentication Error</p>}
+      {!error && isLoading && <p>Loading...</p>}
+      {!error && !isLoading && (
+        <>
+          <LoginButton />
+          <LogoutButton />
+          <Profile />
+        </>
+      )}
     </>
   );
 }
